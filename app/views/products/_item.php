695f2d9d45a1a97019f9e7ea9f57948c92268826
engamer <mohamed.amer2050@gmail.com>
2015-11-25 02:43:04 +0200

    testmonials

--- app/views/products/_item.php
+++ app/views/products/_item.php
@@ -5,7 +5,7 @@
 <li data-tag='postpartum'>
 <!--    <img class="center-block img-responsive" src="images/product-img.jpg">-->
     <?= Html::img($item->thumb(125, 169)) ?>
-	<div class="product-name">name goes here</div>
+	<div class="product-name"><?= $item->title ?></div>
     <?= Html::a(Yii::t('easyii', 'view more').'
         ', ['products/view', 'slug' => $item->slug],['class' => 'btn dry-btn']) ?>
 
--- app/views/products/_itemonline.php
+++ app/views/products/_itemonline.php
@@ -2,7 +2,7 @@
 
 <li>
     <?= Html::img($item->thumb(125, 169),['class'=>'center-block img-responsive']) ?>
-	<div class="product-name">name goes here</div>
+	<div class="product-name"><?= $item->title ?></div>
     <?= Html::a(Yii::t('easyii', 'order online').'', ['products/view-online', 'slug' => $item->slug],['class' => 'btn dry-btn']) ?>
 
 </li>
