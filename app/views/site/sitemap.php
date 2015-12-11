66192bb2d9b0cd8a6e38ebd92b300da4100f2eaf
engamer <mohamed.amer2050@gmail.com>
2015-12-12 01:56:56 +0200

    site map

--- app/views/site/sitemap.php
+++ app/views/site/sitemap.php
@@ -24,23 +24,22 @@ $this->title = $page->seo('title', $page->model->title);
         </div>
         <div class="row">
             <div class="site-map center-block">
-                <a href="#"><i class="fa fa-circle"></i> home</a>
-                <a href="#"><i class="fa fa-circle"></i> About Dry</a>
-                <a href="#"><span><i class="fa fa-circle-o"></i> whos dry</span></a>
-                <a href="#"><span><i class="fa fa-circle-o"></i> awareness program</span></a>
-                <a href="#"><i class="fa fa-circle"></i> Products</a>
-                <a href="#"><i class="fa fa-circle"></i> Offers</a>
-                <a href="#"><i class="fa fa-circle"></i> Find a store</a>
-                <a href="#"><i class="fa fa-circle"></i> contact us</a>
-                <a href="#"><i class="fa fa-circle"></i> order online</a>
+                <a href="/"><i class="fa fa-circle"></i> home</a>
+                <a href="/site/about-dry"><i class="fa fa-circle"></i> About Dry</a>
+                <a href="/site/about-dry"><span><i class="fa fa-circle-o"></i> whos dry</span></a>
+                <a href="/site/awarness"><span><i class="fa fa-circle-o"></i> awareness program</span></a>
+                <a href="/products"><i class="fa fa-circle"></i> Products</a>
+                <a href="/products/offers"><i class="fa fa-circle"></i> Offers</a>
+                <a href="/stores"><i class="fa fa-circle"></i> Find a store</a>
+                <a href="/contact"><i class="fa fa-circle"></i> contact us</a>
+                <a href="/products/online"><i class="fa fa-circle"></i> order online</a>
                 <ul class="social list-inline text-center">
                     <li><a href="#">follow us</a></li>
-                    <li><a href="#"><i class="fa fa-facebook fa-lg"></i></a></li>
-                    <li><a href="#"><i class="fa fa-tumblr fa-lg"></i></a></li>
-                    <li><a href="#"><i class="fa fa-instagram fa-lg"></i></a></li>
-                    <li><a href="#"><i class="fa fa-youtube fa-lg"></i></a></li>
+                    <li><a href="<?= Setting::get('facebook')?>"><i class="fa fa-facebook fa-lg"></i></a></li>
+                    <li><a href="<?= Setting::get('twitter')?>"><i class="fa fa-tumblr fa-lg"></i></a></li>
+                    <li><a href="<?= Setting::get('youtube')?>"><i class="fa fa-youtube fa-lg"></i></a></li>
                 </ul>
-                <a href="#"><i class="fa fa-circle"></i> site map</a>
+                <a href="/site/site-map"><i class="fa fa-circle"></i> site map</a>
             </div>
         </div>
     </div>
