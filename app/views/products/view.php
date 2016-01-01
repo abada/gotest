c191fb655586e3389a455aadc6411c71992f0df3
engamer <mohamed.amer2050@gmail.com>
2016-01-01 14:18:40 +0200

    changes

--- app/views/products/view.php
+++ app/views/products/view.php
@@ -336,7 +336,7 @@ if(Yii::$app->session->getFlash('success') != ''){
                     'itemOptions' => ['class' => 'item'],
                     'itemView' => '_reviewnew',
                     'pager' => ['class' => \kop\y2sp\ScrollPager::className(),
-                        'noneLeftText'=>yii::t('easyii','No More'),
+                        'noneLeftText'=>'',//yii::t('easyii','No More'),
                         'triggerText'=>'<button class="btn dry-btn-2 center-block margin-bottom10">'.yii::t('easyii','More Testimonials').'</button>'],
                 ]);
 
