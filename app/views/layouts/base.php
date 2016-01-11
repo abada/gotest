7ee08c5f9c21083478a77ced4ece3578565b84f1
engamer <mohamed.amer2050@gmail.com>
2016-01-11 22:28:08 +0200

    sharing

--- app/views/layouts/base.php
+++ app/views/layouts/base.php
@@ -18,11 +18,7 @@ use yii\helpers\Html;
         <meta property="og:image" content="<?php echo $this->params['metaimage'] ; ?>" />
         <meta property="og:description" content="<?php echo $this->params['metadesc']; ?>" />
 
-    <body itemscope itemtype="http://schema.org/Article">
-    <h1 itemprop="name"><?php echo $this->params['metatitle']; ?></h1>
-    <img itemprop="image" src="<?php echo $this->params['metaimage'] ; ?>" />
-    <p itemprop="description"><?php echo $this->params['metadesc']; ?></p>
-    </body>
+   
 
 
 
