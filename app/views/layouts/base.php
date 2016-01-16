74198761ce31af50fd5ff67c53ddc5a350bbc92f
engamer <mohamed.amer2050@gmail.com>
2016-01-17 00:22:52 +0200

    changes

--- app/views/layouts/base.php
+++ app/views/layouts/base.php
@@ -7,6 +7,23 @@ use yii\helpers\Html;
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" prefix="og: http://ogp.me/ns#">
     <head>
+
+        <script>
+
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-72514011-1', 'auto');
+
+            ga('send', 'pageview');
+
+        </script>
+
         <meta charset="<?= Yii::$app->charset ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
